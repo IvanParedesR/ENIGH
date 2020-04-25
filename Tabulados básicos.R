1e9f08b6cf6493a302b3d7e47c944f168d27d478
Ivan Paredes <ivanresendiz25@gmail.com>
2020-04-24 23:35:21 -0500

    Local change

--- Tabulados básicos.R
+++ Tabulados básicos.R
@@ -1,11 +1,10 @@
 ##################################################################
 
-#Tabulados b?sicos
+#Tabulados basicos
 
 ##################################################################
 
-#Todas las bases de datos del Modelo Estad?stico 2016 para la continuidad del MCS-ENIGH pueden ser obtenidas en la p?gina de 
-#
+#Todas las bases de datos del Modelo Estad?stico 2016 para la continuidad del ENIGH pueden ser obtenidas en la pagina del INEGI
 
 library(foreign)
 library(car)
@@ -14,6 +13,7 @@ library(reshape)
 library(data.table)
 library(stats)
 
+# limpiamos la base
 rm(list = ls())
 
 #Cargamos la base de hogares
@@ -27,6 +27,8 @@ vivienda <- read.dbf('~/ENIGH/viviendas.dbf',as.is = TRUE)
 
 #unimos bases de datos por medio del folio
 hogares2 = merge(hogares, vivienda,by=c( "folioviv"), all.x = TRUE)
+
+# Convierta textos o tokens a minúsculas (o mayúsculas)
 names(hogares2) =  tolower(names(hogares2))
 
 #ordenamos la nueva base con folioviv
@@ -38,9 +40,8 @@ hogares2$folioviv <- as.numeric(hogares2$folioviv)
 #generamos una variable de entidad
 hogares2$ent=substr(10000000000 + hogares2$folioviv,2,3)
 
-hogares2$ent=substr(10000000000 + hogares2$folioviv,2,3)
-
 hogares2$vivienda_ind = (hogares2$tipo_viv=1)
+
 #### tabla de huespedes
 
 # Nacional
@@ -57,9 +58,17 @@ tabstat_sum[1,6]=sum(hogares2$factor[hogares2$huespedes==5], na.rm=TRUE)
 
 tabstat_sum
 
-#### tIpo vivienda
+#### tipo vivienda
+
+# Valor Etiqueta
+# 1 Casa independiente
+# 2 Departamento en edificio
+# 3 Vivienda en vecindad
+# 4 Vivienda en cuarto de azotea
+# 5 Local no construido para habitación
+
 tabstat_sum=as.data.frame(matrix(0,nr=1,nc=5))
-names(tabstat_sum)[1:5]=cbind("huespedes 0","huespedes 1","huespedes 2","huespedes 3","huespedes 4","huespedes 5")
+names(tabstat_sum)[1:5]=cbind("Casa independiente","Departamento en edificio","Vivienda en vecindad","Vivienda en cuarto de azotea","Local no construido para habitación")
 
 tabstat_sum[1,1]=sum(vivienda$factor[vivienda$tipo_viv==1], na.rm=TRUE)
 tabstat_sum[1,2]=sum(vivienda$factor[vivienda$tipo_viv==2], na.rm=TRUE)
@@ -71,10 +80,9 @@ tabstat_sum
 
 #Estatal 
 
-tabstat_sum=as.data.frame(matrix(0,nr=33,nc=16))
+tabstat_sum=as.data.frame(matrix(0,nr=33,nc=5))
 
-names(tabstat_sum)[1:16]=cbind("pobreza","pobreza_m","pobreza_e","vul_car","vul_ing","no_pobv","carencias","carencias3",
-                               "ic_rezedu","ic_asalud","ic_segsoc","ic_cev","ic_sbv","ic_ali", "plb_m","plb")
+names(tabstat_sum)[1:5]=cbind("c_segsoc","ic_cev","ic_sbv","ic_ali", "plb_m","plb")
 
 row.names(tabstat_sum)[1:33]=cbind("Aguascalientes","Baja California","Baja California Sur","Campeche","Coahuila","Colima"
                                    ,"Chiapas","Chihuahua","Ciudad de México","Durango","Guanajuato","Guerrero","Hidalgo","Jalisco",
